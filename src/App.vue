e4a7e0e5e67faaee70fd5b8db247f82f2686502a
Joaquim Perez <jivkim@gmail.com>
2026-08-28 19:34:22 +0200

    App: add header

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,16 @@ const chronos = ref([
 </script>
 
 <template>
-  <div class="flex min-h-dvh flex-col gap-1 bg-gray-200">
-    <div
-      v-for="chrono in chronos"
-      :key="chrono.id"
-      class="max-h-[25dvh] min-h-20 grow bg-white"
-    >
-      {{ chrono.label }} ({{ chrono.id }})
+  <div class="flex min-h-dvh flex-col">
+    <header class="flex h-12 items-center bg-gray-100 p-2">multichrono</header>
+    <div class="flex grow flex-col gap-1 bg-gray-100">
+      <div
+        v-for="chrono in chronos"
+        :key="chrono.id"
+        class="max-h-1/4 min-h-20 grow bg-white p-2"
+      >
+        {{ chrono.label }} ({{ chrono.id }})
+      </div>
     </div>
   </div>
 </template>
