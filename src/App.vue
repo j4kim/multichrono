e72845523efc98455ce578123e134964632b02da
Joaquim Perez <jivkim@gmail.com>
2026-08-28 23:26:16 +0200

    change colors based on chrono state

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import { chronos } from "./store.js";
 
 <template>
   <div class="flex min-h-dvh flex-col">
-    <header class="flex h-12 items-center bg-gray-100 p-2">multichrono</header>
-    <div class="flex grow flex-col gap-1 bg-gray-100">
+    <header class="flex h-12 items-center bg-slate-950 p-2">multichrono</header>
+    <div class="flex grow flex-col gap-1 bg-slate-950">
       <Chrono
         v-for="(chrono, index) in chronos"
         :key="chrono.id"
--- src/components/Chrono.vue
+++ src/components/Chrono.vue
@@ -1,15 +1,26 @@
 <script setup>
+import { computed } from "vue";
 import { start, pause, stop } from "../store";
 import Time from "./Time.vue";
 
-defineProps({
+const props = defineProps({
   chrono: Object,
   index: Number,
+});
+
+const bgClass = computed(() => {
+  return (
+    {
+      initial: "bg-slate-900",
+      paused: "bg-slate-800",
+      started: "bg-cyan-900",
+    }[props.chrono.state] ?? "bg-red-700"
+  );
 });
 </script>
 
 <template>
-  <div :chrono="chrono" class="max-h-1/4 min-h-20 grow bg-white p-2">
+  <div :chrono="chrono" class="max-h-1/4 min-h-20 grow p-2" :class="bgClass">
     {{ chrono.label }}
     <Time :chrono="chrono"></Time>
     <div class="flex gap-2">
