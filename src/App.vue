38790acc359d57e8f30f88749812ab19ce109331
Joaquim Perez <jivkim@gmail.com>
2026-08-28 19:10:52 +0200

    App: prepare blocks layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="text-2xl">hello</div>
+  <div class="flex min-h-dvh flex-col gap-1 bg-gray-200">
+    <div v-for="i in 4" :key="i" class="max-h-[25dvh] min-h-20 grow bg-white">
+      {{ i }}
+    </div>
+  </div>
 </template>
