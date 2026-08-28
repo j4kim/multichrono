d6490f7a5dad20880b3bed62a4d067286b017cc0
Joaquim Perez <jivkim@gmail.com>
2026-08-28 19:19:34 +0200

    button to add chrono

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const chronos = ref([]);
+
+function addChrono() {
+  chronos.value.push({
+    id: Date.now(),
+    label: chronos.value.length + 1,
+  });
+}
+</script>
 
 <template>
   <div class="flex min-h-dvh flex-col gap-1 bg-gray-200">
-    <div v-for="i in 4" :key="i" class="max-h-[25dvh] min-h-20 grow bg-white">
-      {{ i }}
+    <div
+      v-for="chrono in chronos"
+      :key="chrono.id"
+      class="max-h-[25dvh] min-h-20 grow bg-white"
+    >
+      {{ chrono.label }}
     </div>
+    <button
+      @click="addChrono"
+      class="max-h-[25dvh] min-h-20 grow bg-white text-5xl"
+    >
+      +
+    </button>
   </div>
 </template>
