f748512d450de98b968bc93060e894657d7407d7
Joaquim Perez <jivkim@gmail.com>
2026-08-28 20:19:55 +0200

    new Chrono component

---
+++ src/components/Chrono.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps({
+  chrono: Object,
+});
+</script>
+
+<template>
+  <div :chrono="chrono" class="max-h-1/4 min-h-20 grow bg-white p-2">
+    {{ chrono.label }} ({{ chrono.id }})
+  </div>
+</template>
