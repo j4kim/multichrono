1c3f7bd7398167af5ebd7c5ee83c2ec6ebc4182e
Joaquim Perez <jivkim@gmail.com>
2026-09-04 00:02:27 +0200

    HomveView: new add button in header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup>
 import Chrono from "../components/Chrono.vue";
-import { chronos } from "../store.js";
-import { Cog6ToothIcon } from "@heroicons/vue/24/solid";
+import { add, chronos } from "../store.js";
+import { Cog6ToothIcon, PlusIcon } from "@heroicons/vue/24/solid";
 </script>
 
 <template>
   <div class="flex flex-col">
-    <header class="flex h-12 items-center justify-between p-2 px-4">
+    <header class="flex h-12 items-center justify-between gap-4 p-2 px-4">
       multichrono
+      <div class="grow"></div>
+      <PlusIcon
+        class="size-6 cursor-pointer hover:text-cyan-500"
+        @click="() => add()"
+      />
       <RouterLink to="/settings">
         <Cog6ToothIcon class="size-6 hover:text-cyan-500" />
       </RouterLink>
